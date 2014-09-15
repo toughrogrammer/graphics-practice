d7dd0a87e819e63c91afe454b5435989982a6012
loki <loki@lokiui-MacBook-Air.local>
2014-09-15 18:43:20 +0900

    Initial Commit

---
+++ graphics-pratice/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  graphics-pratice
+//
+//  Created by loki on 2014. 9. 15..
+//  Copyright (c) 2014년 loki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
