9e196cd27ce5ab8e63ae1f3fc90b4d72c5cf6e6c
loki <growingdever@gmail.com>
2014-10-28 15:36:11 +0900

    add Color4f

---
+++ graphics-practice/Base/Color4f.cpp
@@ -0,0 +1,9 @@
+//
+//  Color4f.cpp
+//  graphics-practice
+//
+//  Created by loki on 2014. 10. 28..
+//  Copyright (c) 2014년 loki. All rights reserved.
+//
+
+#include "Color4f.h"
---
+++ graphics-practice/Base/Color4f.h
@@ -0,0 +1,14 @@
+//
+//  Color4f.h
+//  graphics-practice
+//
+//  Created by loki on 2014. 10. 28..
+//  Copyright (c) 2014년 loki. All rights reserved.
+//
+
+#ifndef __graphics_practice__Color4f__
+#define __graphics_practice__Color4f__
+
+#include <stdio.h>
+
+#endif /* defined(__graphics_practice__Color4f__) */
