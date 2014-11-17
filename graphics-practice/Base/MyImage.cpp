3facddc9373760e4c2c0ef666e62d546b2b94b88
loki <growingdever@gmail.com>
2014-11-18 03:06:19 +0900

    no message

--- graphics-practice/Base/MyImage.cpp
+++ graphics-practice/Base/MyImage.cpp
@@ -72,11 +72,6 @@ MyImage* MyImage::LoadImage(string path) {
         return NULL;
     }
     
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    
     image = new MyImage( texture, data, width, height );
     return image;
 }
